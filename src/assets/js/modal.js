7548a4c0a61ce23d62b8a1c348149c83d8b55ca7
Amir <amir-uldashev@mail.ru>
2022-06-28 22:20:30 +0600

    Revert "commit develop 18JS"
    This reverts commit ebc788e65722d9028aae5201bd582de6f928472e.

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -1,56 +1,22 @@
-const modalBtn = document.querySelectorAll('[data-modal]');
+const modalBtn = document.querySelectorAll("[data-modal]");
 const body = document.body;
-const modalClose = document.querySelectorAll('.modal__close');
-const modal = document.querySelectorAll('.modal');
-
+const modalClose = document.querySelectorAll(".modal__close");
 
 modalBtn.forEach(item => {
-    item.addEventListener('click', event => {
+    item.addEventListener("click", event => {
         let $this = event.currentTarget;
-        let modalId = $this.getAttribute('data-modal');
-        let modal = document.getElementById(modalId);
-        let modalContent = modal.querySelector('.modal__content');
-        
-        modalContent.addEventListener('click', event => {
-            event.stopPropagation();
-        });
-        
-        modal.classList.add('show');
-        body.classList.add('no-scroll');
-        
-        setTimeout(() => {
-            modalContent.style.transform = 'none';
-            modalContent.style.opacity = '1';
-        }, 1);
-        
+        let modalId = $this.getAttribute("data-modal");
+        let modal = getElementById(modalId);
+
+        modal.classList.add("show");
+        body.classList.add("no-scroll");
     });
 });
-
-
 modalClose.forEach(item => {
-    item.addEventListener('click', event => {
-        let currentModal = event.currentTarget.closest('.modal');
-        
-        closeModal(currentModal);
+    item.addEventListener("click", event => {
+        let currentModal = event.currentTarget.closest(".modal");
+
+        currentModal.classList.remove("show");
+        body.classList.remove("no-scroll");
     });
 });
-
-
-modal.forEach(item => {
-    item.addEventListener('click', event => {
-        let currentModal = event.currentTarget;
-        
-        closeModal(currentModal);
-    });
-});
-
-
-function closeModal(currentModal) {
-    let modalContent = currentModal.querySelector('.modal__content');
-    modalContent.removeAttribute('style');
-    
-    setTimeout(() => {
-        currentModal.classList.remove('show');
-        body.classList.remove('no-scroll');
-    }, 200);
-}
